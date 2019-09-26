85df45a0477bc8625d958af1de68cb8d1a02c8ac
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-09-26 18:56:57 +0100

    16 compass directions added w/ degrees

---
+++ src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -0,0 +1,21 @@
+package uk.ac.ed.inf.powergrab;
+
+public class Direction {
+	static final double N = 0;
+	static final double NNE = 22.5;
+	static final double NE = 45;
+	static final double ENE = 67.5;
+	static final double E = 90;
+	static final double ESE = 112.5;
+	static final double SE = 135;
+	static final double SSE = 157.5;
+	static final double S = 180;
+	static final double SSW = 202.5;
+	static final double SW = 225;
+	static final double WSW = 247.5;
+	static final double W = 270;
+	static final double WNW = 292.5;
+	static final double NW = 315;
+	static final double NNW = 337.5;
+	
+}
