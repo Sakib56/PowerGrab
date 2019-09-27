5fd19485f7de9f2c94f6155513a1032b98ef04d8
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-09-27 14:23:58 +0100

    angles in radians due to Math.cos(a) requires a to be in radians

--- src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,27 +1,28 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Direction {
-	public double angle;
+	public double theta;
 	
-	public Direction(double angle) {
-		this.angle = angle;
+	public Direction(double theta) {
+		this.theta = theta;
 	}
 	
 	// East was chosen to be 0 degrees to avoid "CAST" calculations
-	static final Direction E = new Direction(0);
-	static final Direction ENE = new Direction(22.5);
-	static final Direction NE = new Direction(45);
-	static final Direction NNE = new Direction(67.5);
-	static final Direction N = new Direction(90);
-	static final Direction NNW = new Direction(112.5);
-	static final Direction NW = new Direction(135);
-	static final Direction WNW = new Direction(157.5);
-	static final Direction W = new Direction(180);
-	static final Direction WSW = new Direction(202.5);
-	static final Direction SW = new Direction(225);
-	static final Direction SSW = new Direction(247.5);
-	static final Direction S = new Direction(270);
-	static final Direction SSE = new Direction(292.5);
-	static final Direction SE = new Direction(315);
-	static final Direction ESE = new Direction(337.5);
+	// Angles in radians due to Math.cos(a) requires a to be in radians
+	static final Direction E  = new Direction(0); 					// 0
+	static final Direction ENE = new Direction(Math.PI * 0.125);	// 22.5
+	static final Direction NE = new Direction(Math.PI * 0.25);		// 45
+	static final Direction NNE = new Direction(Math.PI * 0.375);	// 67.5
+	static final Direction N  = new Direction(Math.PI * 0.5);		// 90
+	static final Direction NNW = new Direction(Math.PI * 0.625);	// 112.5
+	static final Direction NW = new Direction(Math.PI * 0.75);		// 135
+	static final Direction WNW = new Direction(Math.PI * 0.875);	// 157.5
+	static final Direction W  = new Direction(Math.PI);				// 180
+	static final Direction WSW = new Direction(Math.PI * 1.125);	// 202.5
+	static final Direction SW = new Direction(Math.PI * 1.25);		// 225
+	static final Direction SSW = new Direction(Math.PI * 1.375);	// 247.5
+	static final Direction S  = new Direction(Math.PI * 1.5);		// 270
+	static final Direction SSE = new Direction(Math.PI * 1.625);	// 292.5
+	static final Direction SE = new Direction(Math.PI * 1.75);		// 315
+	static final Direction ESE = new Direction(Math.PI * 1.875);	// 337.5
 }
