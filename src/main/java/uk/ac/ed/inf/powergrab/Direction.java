cd6743cd71ca1f97811d67147b65a41a88aa5110
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-09-27 13:40:54 +0100

    east was chosen to be 0 degrees to avoid "CAST" calculations

--- src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,21 +1,27 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Direction {
-	static final double N = 0;
-	static final double NNE = 22.5;
-	static final double NE = 45;
-	static final double ENE = 67.5;
-	static final double E = 90;
-	static final double ESE = 112.5;
-	static final double SE = 135;
-	static final double SSE = 157.5;
-	static final double S = 180;
-	static final double SSW = 202.5;
-	static final double SW = 225;
-	static final double WSW = 247.5;
-	static final double W = 270;
-	static final double WNW = 292.5;
-	static final double NW = 315;
-	static final double NNW = 337.5;
+	public double angle;
 	
+	public Direction(double angle) {
+		this.angle = angle;
+	}
+	
+	// East was chosen to be 0 degrees to avoid "CAST" calculations
+	static final Direction E = new Direction(0);
+	static final Direction ENE = new Direction(22.5);
+	static final Direction NE = new Direction(45);
+	static final Direction NNE = new Direction(67.5);
+	static final Direction N = new Direction(90);
+	static final Direction NNW = new Direction(112.5);
+	static final Direction NW = new Direction(135);
+	static final Direction WNW = new Direction(157.5);
+	static final Direction W = new Direction(180);
+	static final Direction WSW = new Direction(202.5);
+	static final Direction SW = new Direction(225);
+	static final Direction SSW = new Direction(247.5);
+	static final Direction S = new Direction(270);
+	static final Direction SSE = new Direction(292.5);
+	static final Direction SE = new Direction(315);
+	static final Direction ESE = new Direction(337.5);
 }
