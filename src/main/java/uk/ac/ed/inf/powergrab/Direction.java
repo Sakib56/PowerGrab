62597a8c844894f9e87d3673af03a817c365f767
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-10-08 16:02:26 +0100

    changes to vars and fixed issue with lat and long wrong way around

--- src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,28 +1,28 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Direction {
-	public double theta;
+	public double angle;
 	
-	public Direction(double theta) {
-		this.theta = theta;
+	public Direction(double angle) {
+		this.angle = angle;
 	}
 	
 	// East was chosen to be 0 degrees to avoid "CAST" calculations
 	// Angles in radians due to Math.cos(a) requires a to be in radians
 	static final Direction E  = new Direction(0); 					// 0
-	static final Direction ENE = new Direction(Math.PI * 0.125);	// 22.5
-	static final Direction NE = new Direction(Math.PI * 0.25);		// 45
-	static final Direction NNE = new Direction(Math.PI * 0.375);	// 67.5
-	static final Direction N  = new Direction(Math.PI * 0.5);		// 90
-	static final Direction NNW = new Direction(Math.PI * 0.625);	// 112.5
-	static final Direction NW = new Direction(Math.PI * 0.75);		// 135
-	static final Direction WNW = new Direction(Math.PI * 0.875);	// 157.5
+	static final Direction ENE = new Direction(0.125 * Math.PI);	// 22.5
+	static final Direction NE = new Direction(0.25 * Math.PI);		// 45
+	static final Direction NNE = new Direction(0.375 * Math.PI);	// 67.5
+	static final Direction N  = new Direction(0.5 * Math.PI);		// 90
+	static final Direction NNW = new Direction(0.625 * Math.PI);	// 112.5
+	static final Direction NW = new Direction(0.75 * Math.PI);		// 135
+	static final Direction WNW = new Direction(0.875 * Math.PI);	// 157.5
 	static final Direction W  = new Direction(Math.PI);				// 180
-	static final Direction WSW = new Direction(Math.PI * 1.125);	// 202.5
-	static final Direction SW = new Direction(Math.PI * 1.25);		// 225
-	static final Direction SSW = new Direction(Math.PI * 1.375);	// 247.5
-	static final Direction S  = new Direction(Math.PI * 1.5);		// 270
-	static final Direction SSE = new Direction(Math.PI * 1.625);	// 292.5
-	static final Direction SE = new Direction(Math.PI * 1.75);		// 315
-	static final Direction ESE = new Direction(Math.PI * 1.875);	// 337.5
+	static final Direction WSW = new Direction(1.125 * Math.PI);	// 202.5
+	static final Direction SW = new Direction(1.25 * Math.PI);		// 225
+	static final Direction SSW = new Direction(1.375 * Math.PI);	// 247.5
+	static final Direction S  = new Direction(1.5 * Math.PI);		// 270
+	static final Direction SSE = new Direction(1.625 * Math.PI);	// 292.5
+	static final Direction SE = new Direction(1.75 * Math.PI);		// 315
+	static final Direction ESE = new Direction(1.875 * Math.PI);	// 337.5
 }
--- src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -1,25 +1,28 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Position {
-	static final double step = 0.0003; // magnitude r
-	public double longitude; // x component
-	public double latitude; // y component
+	static final double step = 0.0003; 	// magnitude r
+	public double latitude; 			// y component
+	public double longitude; 			// x component
 	
 	public Position(double latitude, double longitude) {
-		this.longitude = longitude;
 		this.latitude = latitude;
+		this.longitude = longitude;
 	}
 	
 	public Position nextPosition(Direction direction) {
-		double newLongitude = this.longitude + step*Math.cos(direction.theta);
-		double newLatitude = this.latitude + step*Math.sin(direction.theta);
-		return new Position(newLongitude, newLatitude);
+		double newLongitude = this.longitude + step*Math.cos(direction.angle);
+		double newLatitude = this.latitude + step*Math.sin(direction.angle);
+		if (!inPlayArea()) { // if NOT in the playable area return old position
+			return new Position(this.latitude, this.longitude);
+		}
+		return new Position(newLatitude, newLongitude); // otherwise return new position
 	}
 	
 	public boolean inPlayArea() {
 		// longitude in [−3.192473, −3.184319] and latitude in [55.942617, 55.946233]
-		boolean withinLong = this.longitude >= -3.192473 && this.longitude <= -3.184319;	
-		boolean withinLat = this.longitude >= 55.942617 && this.longitude <= 55.946233;
+		boolean withinLong = this.longitude > -3.192473 && this.longitude < -3.184319;	
+		boolean withinLat = this.latitude > 55.942617 && this.latitude < 55.946233;
 		return withinLong && withinLat;
 	}
 }
