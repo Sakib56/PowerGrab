c7e85f9292fa71eeec7b97dd06040c1814a843dc
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-11-17 21:41:37 +0000

    reading geojson added

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -1,13 +1,46 @@
 package uk.ac.ed.inf.powergrab;
 
-/**
- * Hello world!!
- *
- */
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        getMapNodeList("2019", "01", "01");
     }
+    
+    public static void getMapNodeList(String year, String month, String day) throws IOException {
+    	month = month.length() == 1 ? "0" + month : month;
+    	day = day.length() == 1 ? "0" + day : day;
+    	String date = year + "/" + month + "/" + day;
+    	String mapString = "http://homepages.inf.ed.ac.uk/stg/powergrab/"
+    						+ date + "/powergrabmap.geojson";
+    	
+    	URL mapUrl = new URL(mapString);
+    	HttpURLConnection conn = (HttpURLConnection) mapUrl.openConnection();
+    	
+    	conn.setReadTimeout(10000);
+    	conn.setConnectTimeout(15000);
+    	conn.setRequestMethod("GET");
+    	conn.setDoInput(true);
+    	conn.connect();
+    	
+    	String mapSource = convertStreamToString(conn.getInputStream());
+    	FeatureCollection fc = FeatureCollection.fromJson(mapSource);
+    	
+    	for (Feature feat : fc.features()) {
+    		System.out.println(feat);
+    	}
+    }
+    
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+    
 }
