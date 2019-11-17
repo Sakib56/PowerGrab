d15f970efc089fe4336762db39aab91daa66dcd3
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-11-17 22:20:45 +0000

    code clean up for geojson to arr

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -11,8 +11,11 @@ import com.mapbox.geojson.Point;
 
 public class App 
 {
-    public static void main( String[] args ) throws IOException
+    private static java.util.Scanner scanner;
+
+	public static void main( String[] args ) throws IOException
     {
+//    	d = new Drone("Stateless");
     	ArrayList<Node> mapNodes = getMapNodeList("2019", "01", "01");
 		for (Node nodes : mapNodes) {
     		System.out.println(nodes);
@@ -20,23 +23,12 @@ public class App
     }
     
     public static ArrayList<Node> getMapNodeList(String year, String month, String day) throws IOException {
-    	month = month.length() == 1 ? "0" + month : month;
-    	day = day.length() == 1 ? "0" + day : day;
-    	String date = year + "/" + month + "/" + day;
-    	String mapString = "http://homepages.inf.ed.ac.uk/stg/powergrab/"
-    						+ date + "/powergrabmap.geojson";
+    	month = month.length() == 1 ? "0"+month : month;
+    	day = day.length() == 1 ? "0"+day : day;
+    	String date = year+"/"+month+"/"+day;
+    	String mapString = "http://homepages.inf.ed.ac.uk/stg/powergrab/"+date+"/powergrabmap.geojson";
     	
-    	URL mapUrl = new URL(mapString);
-    	HttpURLConnection conn = (HttpURLConnection) mapUrl.openConnection();
-    	
-    	conn.setReadTimeout(10000);
-    	conn.setConnectTimeout(15000);
-    	conn.setRequestMethod("GET");
-    	conn.setDoInput(true);
-    	conn.connect();
-    	
-    	String mapSource = convertStreamToString(conn.getInputStream());
-    	FeatureCollection fc = FeatureCollection.fromJson(mapSource);
+    	FeatureCollection fc = connectToInfServer(mapString);
     	
     	ArrayList<Node> mapNodes = new ArrayList<Node>();
     	for (Feature f : fc.features()) {
@@ -53,8 +45,23 @@ public class App
     	return mapNodes;
     }
     
+    public static FeatureCollection connectToInfServer(String mapString) throws IOException {
+    	URL mapUrl = new URL(mapString);
+    	HttpURLConnection conn = (HttpURLConnection) mapUrl.openConnection();
+    	
+    	conn.setReadTimeout(10000);
+    	conn.setConnectTimeout(15000);
+    	conn.setRequestMethod("GET");
+    	conn.setDoInput(true);
+    	conn.connect();
+    	
+    	String mapSource = convertStreamToString(conn.getInputStream());
+    	return FeatureCollection.fromJson(mapSource);
+    }
+    
     static String convertStreamToString(java.io.InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        scanner = new java.util.Scanner(is);
+		java.util.Scanner s = scanner.useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
     }
     
