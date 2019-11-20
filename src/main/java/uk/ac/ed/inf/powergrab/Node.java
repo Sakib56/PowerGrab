0556a6aad14c801a6e24b0051bb869cab814d2ed
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-11-20 21:52:21 +0000

    overloading for tempFromPos

--- src/main/java/uk/ac/ed/inf/powergrab/Node.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Node.java
@@ -1,14 +1,13 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Node {
-	static final double r = 0.00025; 	// radius of effect
+	private static final double r = 0.00025; 	// radius of effect
 	public double weight;
 	public double coins;
 	public double power;
 	public String type;
 	public Position pos;
-
-	
+	public Position tempFromPos = null;
 	
 	public Node(float coins, float power, String type, Position pos) {
 		this.coins = coins;
@@ -18,8 +17,21 @@ public class Node {
 		this.weight = coins + power;
 	}
 	
+	public Node(float coins, float power, String type, Position pos, Position tempFromPos) {
+		this.coins = coins;
+		this.power = power;
+		this.type = type;
+		this.pos = pos;
+		this.weight = coins + power;
+		this.tempFromPos = tempFromPos;
+	}
+	
 	public String toString() {
-		return "<coins:" + this.coins + ", power:" + this.power + ", type:" + this.type
-				+ ", pos(" + this.pos.latitude + ", " + this.pos.longitude + ")>";
+		return "<coins:"+this.coins+", power:"+this.power+", type:"+this.type+", weight:"+
+				this.weight+", "+pos.toString()+")>";
+	}
+
+	public double getRadius() {
+		return r;
 	}
 }
