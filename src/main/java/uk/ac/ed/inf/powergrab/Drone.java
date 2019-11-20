714160e808368789f28861ed333e9f2813154d2a
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-11-20 19:18:05 +0000

    current(Power and Coins) + toString added

--- src/main/java/uk/ac/ed/inf/powergrab/Drone.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Drone.java
@@ -4,16 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Drone {
+	static final float powerConsump = (float) -1.25;
+	public float currentPower;
+	public float currentCoins;
 	public Position currentPos;
 	public ArrayList<Position> posChoices;
 
 	public Drone(Position pos) {
 		this.currentPos = pos;
+		this.currentPower = 250;
+		this.currentCoins = 0;
 		this.posChoices = getNextMoves(); 
 	}
 	
-	public void moveTo(Position pos) {
+	public void moveTo(Position pos, float extraPower, float extraCoins) {
+		System.out.println(toString());
 		this.currentPos = pos;
+		this.currentPower += (extraPower + powerConsump);
+		this.currentCoins += extraCoins;
 		this.posChoices = getNextMoves();
 	}
 	
@@ -25,5 +33,11 @@ public class Drone {
 			posChoices.add(this.currentPos.nextPosition(dir));
 		}
 		return posChoices;
+	}
+	
+	public String toString() {
+		String state = currentPower <= 0 ? "DEAD" : "ALIVE";
+		return "<state:"+state+", coins:"+this.currentCoins+", power: "+this.currentPower+
+				", pos("+this.currentPos.latitude +", "+ this.currentPos.longitude+")>";
 	}
 }
