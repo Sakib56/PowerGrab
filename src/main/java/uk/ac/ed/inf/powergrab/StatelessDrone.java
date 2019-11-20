f7a238d1a04b76d8ed328408e851b538afa210b9
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-11-20 19:18:35 +0000

    comments explaining approach

--- src/main/java/uk/ac/ed/inf/powergrab/StatelessDrone.java
+++ src/main/java/uk/ac/ed/inf/powergrab/StatelessDrone.java
@@ -11,18 +11,16 @@ public class StatelessDrone extends Drone {
 	}
 
 	public FeatureCollection play(ArrayList<Node> mapNodes) {
-		ArrayList<Position> bestNextPos = new ArrayList<Position>(); // bestNextPos (needs maintained)
+		ArrayList<Position> bestNextPos = new ArrayList<Position>(); // best next pos to move to (needs maintained)
 		
 		for (Node node : mapNodes) { // loop through all nodes
-			double fromCurrDist = this.currentPos.getL2Dist(node.pos);
-			
-			
-			if (fromCurrDist <= this.currentPos.step + node.r) { //inside big circle
+			double fromCurrDist = this.currentPos.getL2Dist(node.pos); // calc dist from currentPos to each node, n
+			if (fromCurrDist <= this.currentPos.step + node.r) { // inside big circle, n falls in threshold (step+roe)
 				
-				for (Position pos : this.posChoices) {
-					double fromNextPosDist = pos.getL2Dist(node.pos);
+				for (Position pos : this.posChoices) { // loop through all next possible pos's
+					double fromNextPosDist = pos.getL2Dist(node.pos); // get dist from next pos to that node, n
 					if (fromNextPosDist <= node.r) { // in AOE of one of the nodes (inside big circle)
-						bestNextPos.add(pos);
+						bestNextPos.add(pos); // add to next pos to move to (need maintained)
 					}
 				}
 			}
