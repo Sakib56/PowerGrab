81fa31730f59490348a3ebce8e02717a0caa4040
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-11-21 19:54:36 +0000

    use var added and getWeight method for ease of change

--- src/main/java/uk/ac/ed/inf/powergrab/Node.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Node.java
@@ -2,19 +2,20 @@ package uk.ac.ed.inf.powergrab;
 
 public class Node {
 	private static final double r = 0.00025; 	// radius of effect
-	public double weight;
-	public double coins;
-	public double power;
+	public boolean used = false;
+	public Position tempFromPos = null;	// temp buffer to store a pos that drone can get to s.t. in radius of this node
+	public double weight = 0;
+	public double coins = 0;
+	public double power = 0;
 	public String type;
 	public Position pos;
-	public Position tempFromPos = null;
 	
 	public Node(float coins, float power, String type, Position pos) {
 		this.coins = coins;
 		this.power = power;
 		this.type = type;
 		this.pos = pos;
-		this.weight = coins + power;
+		this.weight = getWeight();
 	}
 	
 	public Node(float coins, float power, String type, Position pos, Position tempFromPos) {
@@ -22,16 +23,20 @@ public class Node {
 		this.power = power;
 		this.type = type;
 		this.pos = pos;
-		this.weight = coins + power;
+		this.weight = getWeight();
 		this.tempFromPos = tempFromPos;
 	}
 	
 	public String toString() {
 		return "<coins:"+this.coins+", power:"+this.power+", type:"+this.type+", weight:"+
-				this.weight+", "+pos.toString()+")>";
+				this.weight+", used:"+this.used+", "+pos.toString()+")>";
 	}
 
 	public double getRadius() {
 		return r;
+	}
+
+	public double getWeight() {
+		return this.coins + this.power;
 	}
 }
