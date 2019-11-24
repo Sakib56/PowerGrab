afb9755fdc01a3329bae6b80b9f35121af1db503
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-11-24 16:31:23 +0000

    print path and useNode, moveTo updated

--- src/main/java/uk/ac/ed/inf/powergrab/Drone.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Drone.java
@@ -9,22 +9,27 @@ public class Drone {
 	public float currentCoins;
 	public Position currentPos;
 	public ArrayList<Position> posChoices;
+	public ArrayList<Position> movesMadeSoFar = new ArrayList<Position>();
 
 	public Drone(Position initPos) {
 		this.currentPos = initPos;
 		this.currentPower = 250;
 		this.currentCoins = 0;
 		this.posChoices = getNextMoves(); 
+		this.movesMadeSoFar.add(initPos);
 	}
 	
 	public void moveTo(Position pos) {
-		System.out.println(toString());
-		this.currentPos = pos;
+		if (pos.inPlayArea() && this.currentPower > 0) {
+			this.currentPower += powerConsump;
+			this.currentPos = pos;
+			this.movesMadeSoFar.add(pos);
+		}
 		this.posChoices = getNextMoves();
 	}
 	
 	public void use(Node node) {
-		this.currentPower += (node.power + powerConsump);
+		this.currentPower += (node.power + powerConsump); //do you consume power if you use a node??
 		this.currentCoins += node.coins;
 		
 		node.coins = 0;
@@ -43,9 +48,36 @@ public class Drone {
 		return posChoices;
 	}
 	
+	public void printPath() {
+    	String coordinates = "";
+    	for (int i=0; i<this.movesMadeSoFar.size(); i++) {
+    		Position pos = this.movesMadeSoFar.get(i);
+    		String coord = "          ["+pos.longitude+", "+pos.latitude+"]";
+			
+			if (i == this.movesMadeSoFar.size()-1) {
+				coordinates += coord+"\r\n";
+			} else {
+				coordinates += coord+",\r\n";
+			}
+    	}
+    	
+    	System.out.println("    {\r\n" + 
+    			"      \"type\": \"Feature\",\r\n" + 
+    			"      \"properties\": { },\r\n" + 
+    			"      \"geometry\": {\r\n" + 
+    			"        \"type\": \"LineString\",\r\n" + 
+    			"        \"coordinates\": [\r\n" + 
+    			coordinates +
+    			"        ]\r\n" + 
+    			"      }\r\n" + 
+    			"    },");	
+
+//    	System.out.println("\n"+d.toString());
+	}
+	
 	public String toString() {
 		String state = currentPower <= 0 ? "DEAD" : "ALIVE";
-		return "<state:"+state+", coins:"+this.currentCoins+", power: "
-				+this.currentPower+", "+this.currentPos.toString()+">";
+		return "<state:"+state+", coins:"+this.currentCoins+", power:"
+				+this.currentPower+", #moves:"+this.movesMadeSoFar.size()+", "+this.currentPos.toString()+">";
 	}
 }
